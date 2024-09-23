b3d6c1d8482093873b43d48652b6e861278c153b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-09-23 20:26:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Results from "./components/Results";
 import UserInput from "./components/UserInput";
 
+const INVESTMENTS = {
+  initialInvestment: 10000,
+  annualInvestment: 1200,
+  expectedReturns: 6,
+  duration: 10,
+};
+
 function App() {
+  const [userInput, setUserInput] = useState(INVESTMENTS);
+
+  const handleUserInputChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  };
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onInputChange={handleUserInputChange} />
+      <Results userInput={userInput} />
     </>
   );
 }
